6d43f588c8e1c23dce5ba5f9b62b0c5bed51f878
Obluchatel <azamatovartem@gmail.com>
2026-01-23 15:01:49 +0100

    PEP8

--- exceptions.py
+++ exceptions.py
@@ -3,20 +3,16 @@ class ConfigurationError(Exception):
     """Raised when a configuration file is not found."""
 
 
-
 class ConfValidationError(ConfigurationError):
     """Raised when a configuration file is invalid."""
-
 
 
 class PiHoleError(Exception):
     """Base exception for all Pi-hole related errors."""
 
 
-
 class PiHoleAPIError(PiHoleError):
     """Raised when a Pi-hole API fails."""
-
 
 
 class PiHoleBackupError(PiHoleError):

bef6889356431659d84200dc46f67dc698634e97
Obluchatel <azamatovartem@gmail.com>
2026-02-07 10:36:46 +0100

    Reformat

--- logger.py
+++ logger.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_logger():
     logger = logging.getLogger(__name__)
-    BASE_DIR = os.getenv("BASE_DIR", os.getcwd())
-    LOG_FILE = os.path.join(BASE_DIR, "backup_upgrade.log")
-    logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s",
-                    filename=LOG_FILE,
-                    level=logging.INFO,
-                    datefmt="[%Y-%m-%d %H:%M:%S]")
+    base_dir = os.getenv("BASE_DIR", os.getcwd())
+    log_file = os.path.join(base_dir, "backup_upgrade.log")
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s", filename=log_file, level=logging.INFO,
+                        datefmt="[%Y-%m-%d %H:%M:%S]")
     return logger
